654e9083593b41a435d739cf7431a271baeed182
Joshua Chapman <joshua.chapman@joshuachp.dev>
2025-01-05 15:12:37 +0100

    feat(restic): create restic container

---
+++ restic/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -exEuo pipefail
+
+git fetch --tags
+
+tag="$(
+    git tag |
+        grep '^restic-' |
+        sed -e 's/^restic-//' |
+        sort -V |
+        tail -n1
+)"
+
+docker buildx build \
+    --builder=container \
+    --platform=linux/amd64,linux/arm64 \
+    -t "joshuachp/restic:$tag" \
+    -f Dockerfile \
+    --push .
